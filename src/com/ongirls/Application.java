fc9a243043587c7fc918fab4c0f809484fc90f96
SeoYeonUser <idxuse@gmail.com>
2023-04-21 16:49:38 +0900

    수정2

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -5,6 +5,8 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
 	}
 
 }

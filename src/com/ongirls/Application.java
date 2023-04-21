5a93469b87bbe24d2fe2f704deb134362d1d0135
DESKTOP-EM2UUI3\chohyeran <gpfks765@gmail.com>
2023-04-21 17:19:50 +0900

    수정하기

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("수정 후 push할때 원본(master)랑 다르게 저장하기!!!!!!!!1111111111111");
+		System.out.println("수정 후 push할때 원본(master)ㅇㄴㄹㅇㄴㄹ");
 		
 		/*파일 하나를 두고 작업하면 충돌*/	
 	}

401f3bb944867e28296863b6aab6d57925544106
jieun kwon <midi4214@gmail.com>
2023-04-21 16:43:07 +0900

    first

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -3,8 +3,7 @@ package com.ongirls;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("테스ㅇ트");
+		System.out.println("안녕하세요!");
 	}
 
 }

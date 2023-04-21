a764818dbe284732073e770bade5a95e4d774753
DESKTOP-EM2UUI3\chohyeran <gpfks765@gmail.com>
2023-04-21 17:37:38 +0900

    혜란 수정했습니다.

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -11,5 +11,13 @@ public class Application {
 		System.out.println("소스수정했습니다.");
 		System.out.println("소스수정했습니다.");
 		System.out.println("강서연소스수정했습니다.");
+		
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
+		System.out.println("수정 1번 혜란이 했다");
 	}
 }

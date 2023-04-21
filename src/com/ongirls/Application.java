a8aeb0746ea598e7a468111e7877dd3e5542cb22
SeoYeonUser <idxuse@gmail.com>
2023-04-21 18:11:14 +0900

    마지막

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -3,7 +3,10 @@ package com.ongirls;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("저도 수정했어요");
+
+
+		System.out.println("집에갈시간");
+		
+
 	}
 }

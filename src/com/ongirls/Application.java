1cfa74f20716e96759ae2e9b6a4b48d85dd139ed
SeoYeonUser <idxuse@gmail.com>
2023-04-21 17:35:40 +0900

    2023.04.21

--- src/com/ongirls/Application.java
+++ src/com/ongirls/Application.java
@@ -4,12 +4,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("테스트");
-		System.out.println("테스트");
-		System.out.println("테스트");
-		System.out.println("소스수정했습니다.");
-		System.out.println("소스수정했습니다.");
-		System.out.println("소스수정했습니다.");
-		System.out.println("강서연소스수정했습니다.");
+		System.out.println("강서연 배열 오류 수정");
 	}
 }
